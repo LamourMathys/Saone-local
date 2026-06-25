4a6f332d7b2e908cdca572445f59939a5a27c316
lamourmathys <mathys.lamoour@gmail.com>
2026-06-25 17:53:31 +0200

    feat(comment): mentions legales

--- client/src/app/mentions-legales/page.tsx
+++ client/src/app/mentions-legales/page.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 
 export default function Mentions() {
-  return <main className="w-full">
-              {/* bloc "mention legal" en haut à droite à ajouter */}
-              <div className="relative w-full aspect-3/1">
-                <Image
-                  src="/banniere-SaoneLocal.svg"
-                  alt="Label Fabriqué en Bourgogne"
-                  fill
-                  priority
-                  className="object-contain"
-                />
-              </div>
-          <div>
-            VOICI DES MAGNIFIQUES MENTIONS LEGALES
-          </div>
-          </main>;
+  return (
+    <main className="w-full">
+      {/* bloc "mention legal" en haut à droite à ajouter */}
+      {/* faire devenir un components pour le reutiliser plus efficacement  */}
+      <div className="relative w-full aspect-3/1">
+        <Image
+          src="/banniere-SaoneLocal.svg"
+          alt="Label Fabriqué en Bourgogne"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+      <div>VOICI DES MAGNIFIQUES MENTIONS LEGALES</div>
+    </main>
+  );
 }

181ccd28726feaf18408c35c1aabf1a6af4600aa
lamourmathys <mathys.lamoour@gmail.com>
2026-07-01 15:46:37 +0200

    fix(/users):access

--- server/routes/usersroute.js
+++ server/routes/usersroute.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const userscontroller = require("../controllers/userscontroller");
 
 const { verifyAdmin } = require("../verification/adminverification");
+const { verifyAuth } = require("../verification/authverification");
 
-router.get("/", userscontroller.getAllUsers);
-router.get("/:id", userscontroller.getUserById);
-router.post("/", verifyAdmin, userscontroller.createUser);
-router.put("/:id", verifyAdmin, userscontroller.updateUser);
-router.delete("/:id", verifyAdmin, userscontroller.deleteUser);
+router.get("/", verifyAuth, verifyAdmin, userscontroller.getAllUsers);
+router.get("/:id", verifyAuth, verifyAdmin, userscontroller.getUserById);
+router.post("/", verifyAuth, verifyAdmin, userscontroller.createUser);
+router.put("/:id", verifyAuth, verifyAdmin, userscontroller.updateUser);
+router.delete("/:id", verifyAuth, verifyAdmin, userscontroller.deleteUser);
 
 module.exports = router;

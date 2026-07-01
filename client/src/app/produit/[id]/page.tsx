acdd7c607c92bc14e452367ebfa2e57e698e678a
lamourmathys <mathys.lamoour@gmail.com>
2026-07-01 18:50:58 +0200

    feat(front):started working on product page

--- client/src/app/produit/[id]/page.tsx
+++ client/src/app/produit/[id]/page.tsx
@@ -1,3 +1,15 @@
+import ProfileHeader from "../../../components/boxandbouton-component/boxandbouton";
+
 export default function produit() {
-  return <main className="w-full">produit</main>;
+  return (
+    <main className="w-full">
+      <div className="pt-4 px-4 flex flex-col gap-4">
+        <ProfileHeader
+          text="produit"
+          boxColor="bg-[#8B362E]"
+          buttonColor="bg-[#8B362E]"
+        />
+      </div>
+    </main>
+  );
 }
